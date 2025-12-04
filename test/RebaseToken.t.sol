1cad6dec63bf44999968f96179e47219a54a5e20
João Victor Lyra <jvmlyra@gmail.com>
2025-12-04 09:28:03 -0300

    feat: start of test suite

---
+++ test/RebaseToken.t.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+import {Test, console} from "forge-std/Test.sol";
+import {RebaseToken} from "../src/RebaseToken.sol";
+import {Vault} from "../src/Vault.sol";
+import {IRebaseToken} from "../src/interfaces/IRebaseToken.sol";
+
+contract RebaseTokenVaultTest is Test {
+    RebaseToken private rebaseToken;
+    Vault private vault;
+
+    address public owner = makeAddr("owner");
+    address public user = makeAddr("user");
+
+    function setUp() public {
+        vm.startPrank(owner);
+        rebaseToken = new RebaseToken();
+        vault = new Vault(IRebaseToken(address(rebaseToken)));
+        rebaseToken.grantMintAndBurnRole(address(vault));
+        (bool success, ) = payable(address(vault)).call{value: 1e18}("");
+        vm.stopPrank();
+    }
+
+    function testDepositLinear(uint256 amount) public {
+        vm.assume(amount > 1e5);
+        amount = bound(amount, 1e5, type(uint96).max); 
+        // 1. deposit
+        vm.startPrank(user);
+        vm.deal(user, amount);
+        // 2. check our rebase token balance
+        // 3. warp the time and check the balance again
+        // 4. warp the time again by the same amount and check the balance again
+    }
+}
