ab9f778a06e8f88de543b8effa6b4e3a3326854c
João Victor Lyra <jvmlyra@gmail.com>
2025-12-10 11:49:38 -0300

    feat: chainlink local and fork tests

---
+++ test/CrossChain.t.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+import {Test, console} from "forge-std/Test.sol";
+
+import {CCIPLocalSimulatorFork} from "@chainlink/local/src/ccip/CCIPLocalSimulatorFork.sol";
+
+import {RebaseToken} from "../src/RebaseToken.sol";
+import {RebaseTokenPool} from "../src/RebaseTokenPool.sol";
+import {Vault} from "../src/Vault.sol";
+
+import {IRebaseToken} from "../src/interfaces/IRebaseToken.sol";
+
+contract CrossChainTest is Test {
+    uint256 sepoliaFork;
+    uint256 arbSepoliaFork;
+
+    CCIPLocalSimulatorFork ccipLocalSimulatorFork;
+
+    function setUp() public {
+        sepoliaFork = vm.createSelectFork("sepolia");
+        arbSepoliaFork = vm.createFork("arb-Sepolia");
+
+        ccipLocalSimulatorFork = new CCIPLocalSimulatorFork();
+        vm.makePersistent(address(ccipLocalSimulatorFork));
+    }
+}
+
